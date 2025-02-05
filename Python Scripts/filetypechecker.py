6e7e0877b86d41660710cb10a391fbd42cd9498b
smokeshard <smokeshard@protonmail.com>
2025-02-05 20:41:14 +0530

    Update filetypechecker.py

--- Python Scripts/filetypechecker.py
+++ Python Scripts/filetypechecker.py
@@ -8,18 +8,18 @@ def getMIME(filePath):
 # Next, check the file's provided extension.
 def checkExtension(filePath):
   if not os.path.isfile(filePath):
-    return f"[ERR.] '{filePath}' does not exist."
+    return f"[ERR.] '{filePath}' does not exist.\n"
   fileExtension, _ = mimetypes.guess_type(filePath)
   if fileExtension is None:
-    return f"[ERR.] '{filePath}' MIME type could not be determined."
+    return f"[ERR.] '{filePath}' MIME type could not be determined.\n"
   with open(filePath, 'rb') as f:
     fileHexcode = f.read(4).hex().upper()
   knownMIME = {"89504E47": "image/png", "FFD8FFDB": "image/jpeg", "FFD8FFE0": "image/jpeg", "FFD8FFE1": "image/jpeg", "504B0304": "application/zip",}
   fileMIME = knownMIME.get(fileHexcode, "Unknown")
   if fileMIME == fileExtension:
-    return f"[SUCC] '{filePath}' is of type ({fileMIME}) and has a matching extension."
+    return f"[SUCC] '{filePath}' is of type ({fileMIME}) and has a matching extension.\n"
   else:
-    return f"[FAIL] '{filePath}' is of type ({fileMIME}), but has an extension claiming ({fileExtension})."
+    return f"[FAIL] '{filePath}' is of type ({fileMIME}), but has an extension claiming ({fileExtension}).\n"
 
 if __name__ == "__main__":
   filePath = input("\nInput an absolute file path: ")
